874fea34b5204ba8a8acaf4c624508a758b7dae8
sroy3333 <121767970+sroy3333@users.noreply.github.com>
2024-06-26 02:16:11 +0530

    Update groupMember.js

--- routes/groupMember.js
+++ routes/groupMember.js
@@ -5,5 +5,5 @@ const authenticate = require('../middleware/auth');
 
 router.post('/invite', authenticate, groupMemberController.inviteMember);
 router.post('/remove', authenticate, groupMemberController.removeMember);
-
+router.post('/promote', authenticate, groupMemberController.promoteMember);
 module.exports = router;
